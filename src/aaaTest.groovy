03013723802dec32a77511dd33eceee7af7a78cc
Eiji Ito <aeffy7+git@gmail.com>
2012-09-01 12:53:25 +0900

    変更した。

--- src/aaaTest.groovy
+++ src/aaaTest.groovy
@@ -1,19 +1,12 @@
 import spock.lang.Specification
 
-/**
- * Created with IntelliJ IDEA.
- * User: TDD
- * Date: 12/09/01
- * Time: 1:53
- * To change this template use File | Settings | File Templates.
- */
 class aaaTest extends Specification {
 
-
+    // 変更
     def "SimpleTest"() {
         when:
            def a = 1
         then:
-            assert (a++ == 2)
+            assert (a++ == 2*(a++)*(a++))
     }
 }
